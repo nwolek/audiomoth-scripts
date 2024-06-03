0e903b433264e4f6a53a7d34ff2d1b784ec816fc
Nathan Wolek <nw@nathanwolek.com>
2024-06-03 14:36:00 -0400

    update rename-by-date to follow official AudioMoth filename pattern (YYYYMMDD_HHMMSS.wav)

--- rename-by-date.sh
+++ rename-by-date.sh
@@ -6,7 +6,7 @@
 
 # BEGUN - 23 April 2020
 # GOAL - change the WAV filename to be based on the birthdate
-# Example of desired format - 2020-04-06-04-00-00-UTC
+# Example of desired format - 20200406_040000
 # expected input .WAV files (upper case extension) produced by the AudioMoth
 # expected output .wav files (lower case extension)
 
@@ -21,9 +21,9 @@ do
 
 	# Use 'stat' to get the birthdate of the file, but switch the format
 	# Example of default format from stat - Apr  6 04:00:00 2020
-	# Example of desired format - 2020-04-06-04-00-00-UTC
+	# Example of desired format - 20200406_040000
 
-	format='%F-%H-%M-%S-UTC'
+	format='%Y%m%d_%H%M%S'
 	birthdate=$(stat -f "%SB" -t $format $file)
 
 	# copy the original sound file and give it a new birthdate name
